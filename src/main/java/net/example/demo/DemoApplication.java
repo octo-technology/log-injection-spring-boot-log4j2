3ef81fa135a84009e6d2544f59a8fc3ce5e47f30
marie-bisault <65164427+marie-bisault@users.noreply.github.com>
2024-10-18 16:05:52 +0200

    Rebase fix after improved solution (#5)
    * exclude spring-boot-starter-logging for tests
    
    * Improve demo (#4)
    
    * feat: add exploits to call real Java classes
    
    * fix: USER env variable and keepass command, depending on os
    
    * fix: netcat command
    
    * fix: using JAVA_HOME env variable
    
    * use java even if JAVA_HOME is not set
    
    * update readme description
    
    ---------
    
    Co-authored-by: Victor Nea <victor.nea@octo.com>
    Co-authored-by: Elisabeth Czostkiewicz <e.abbas.zadeh.namini@octo.com>
    Co-authored-by: Marie Bisault <marie_17_31@yahoo.fr>
    
    * feat: add exploits to call real Java classes
    
    * fix: USER env variable and keepass command, depending on os
    
    * add solution
    
    * rebase
    
    * clean build.gradle
    
    ---------
    
    Co-authored-by: Marie Bisault <marie_17_31@yahoo.fr>
    Co-authored-by: Victor Nea <victor.nea@octo.com>
    Co-authored-by: Elisabeth Czostkiewicz <e.abbas.zadeh.namini@octo.com>

--- src/main/java/net/example/demo/DemoApplication.java
+++ src/main/java/net/example/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase", "true");
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
